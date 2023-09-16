a9420c060f71f82cb0ff1d427a48774dd7ee9327
Murtaza usmani <murtazausmani985@gmail.com>
2023-09-16 16:01:54 +0500

    RegistrationSuccessful However Error occuring

--- InstituteOfFineArts/Controllers/AccountController.cs
+++ InstituteOfFineArts/Controllers/AccountController.cs
@@ -17,6 +17,7 @@ namespace InstituteOfFineArts.Controllers
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
+        ApplicationDbContext _db = new ApplicationDbContext();
 
         public AccountController()
         {
@@ -139,6 +140,7 @@ namespace InstituteOfFineArts.Controllers
         [AllowAnonymous]
         public ActionResult Register()
         {
+            ViewBag.Name = new SelectList(_db.Roles.ToList(), "Name", "Name");
             return View();
         }
 
@@ -155,14 +157,14 @@ namespace InstituteOfFineArts.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    await UserManager.AddToRoleAsync(user.Id, model.Name);
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
                     return RedirectToAction("Index", "Home");
                 }
                 AddErrors(result);
--- InstituteOfFineArts/Models/AccountViewModels.cs
+++ InstituteOfFineArts/Models/AccountViewModels.cs
@@ -79,6 +79,10 @@ namespace InstituteOfFineArts.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
     }
 
     public class ResetPasswordViewModel
